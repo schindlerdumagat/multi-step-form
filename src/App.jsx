a80f3f8d4d137eaa12097659485301de52d72e75
schindlerwork <schindler@xtendly.com>
2026-05-20 20:01:02 +0800

    Add: tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+    <>
+      <h1 className="font-bold text-2xl/tight text-blue-950 md:text-3xl/tight">Personal info</h1>
+      <p className="font-normal text-base/normal text-grey-500">Please provide your name, email address, and phone number.</p>
+    </>
   )
 }
 
